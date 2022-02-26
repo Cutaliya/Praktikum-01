6ce839807e04157d4d701a31377c61936c322625
Cutaliya <Cutaliya72@gmail.com>
2022-02-25 17:30:16 -0800

    menambahkan folder Praktikum 01

---
+++ variabel-user.php
@@ -0,0 +1,12 @@
+<?php
+// definisikan variable
+$nama = "Cut Aliya";
+$umur = 13;
+$berat = 35.5;
+
+echo "Nama : $nama ";
+echo "<br>Umur : $umur Tahun ";
+echo "<br>Berat : $berat Kg ";
+
+echo "<br> Hello $nama Apa kabar";
+?>
